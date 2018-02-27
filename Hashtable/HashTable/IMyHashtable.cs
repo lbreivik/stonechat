2d8c15b05d4bdda2656bcf5717c49031031c5099
Louise Breivik <lbreivik@zarion.com>
2018-02-27 12:16:28 +0000

    new hashtable

---
+++ Hashtable/HashTable/IMyHashtable.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public interface IMyHashtable<TKey, TValue>
+    {
+        TValue Find(TKey key);
+        void Add(TKey key, TValue value);
+        void Remove(TKey key);
+        int Count();
+    }
+
+
+    public class MyHashtable<TKey, TValue> : IMyHashtable<TKey, TValue>
+    {
+
+        private readonly int size;
+        internal readonly LinkedList<KeyValuePair<TKey, TValue>>[] items;
+
+        public MyHashtable(int size)
+        {
+            this.size = size;
+            items = new LinkedList<KeyValuePair<TKey, TValue>>[size];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<TKey, TValue>> linkedList = GetLinkedList(position);
+            KeyValuePair<TKey, TValue> item = new KeyValuePair<TKey, TValue>(key, value);
+            linkedList.AddLast(item);
+        }
+
+        public TValue Find(TKey key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<TKey, TValue>> linkedList = GetLinkedList(position);
+            foreach (KeyValuePair<TKey, TValue> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+
+            return default(TValue);
+        }
+
+        public void Remove(TKey key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<TKey, TValue>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValuePair<TKey, TValue> foundItem = default(KeyValuePair<TKey, TValue>);
+            foreach (KeyValuePair<TKey, TValue> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+
+        internal int GetArrayPosition(TKey key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        protected LinkedList<KeyValuePair<TKey, TValue>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValuePair<TKey, TValue>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValuePair<TKey, TValue>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public int Count()
+        {
+            int counter = 0;
+            foreach(var bucket in items)
+            {
+                if (bucket != null)
+                {
+                    counter += bucket.Count;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ Hashtable/TestHashtable/TestMyHashtable.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using HashTable;
+
+namespace TestHashtable
+{
+    [TestFixture]
+    public class TestMyHashtable
+    {
+        [Test]
+        public void MyHashtable_Add5_succeeds()
+        {
+            var hashtbl = new MyHashtable<string, Person>(10);
+            var p = new Person() { Name = "Louise", City = "Dublin" };
+            var p2 = new Person() { Name = "Mary", City = "Dublin" };
+            var p3 = new Person() { Name = "John", City = "Dublin" };
+            var p4 = new Person() { Name = "Louise", City = "Cork" };
+            var p5 = new Person() { Name = "Gary", City = "Galway" };
+
+            hashtbl.Add(p.Name, p);
+            hashtbl.Add(p2.Name, p2);
+            hashtbl.Add(p3.Name, p3);
+            hashtbl.Add(p4.Name, p4);
+            hashtbl.Add(p5.Name, p5);
+
+            Assert.AreEqual(hashtbl.Count(), 5);
+        }
+
+        [Test]
+        public void MyHashtable_Add2_succeeds()
+        {
+            var hashtbl = new MyHashtable<string, Person>(10);
+            var p = new Person() { Name = "Louise", City = "Dublin" };
+            var p2 = new Person() { Name = "Mary", City = "Dublin" };
+
+            hashtbl.Add(p.Name, p);
+            hashtbl.Add(p2.Name, p2);
+
+            Assert.AreEqual(hashtbl.Count(), 2);
+        }
+
+        [Test]
+        public void MyHashtable_Add1_Find1_succeeds()
+        {
+            var hashtbl = new MyHashtable<string, Person>(10);
+            var p = new Person() { Name = "Louise", City = "Dublin" };
+
+            hashtbl.Add(p.Name, p);
+            var result = hashtbl.Find(p.Name);
+
+            Assert.AreEqual(result.Name, p.Name);
+            Assert.AreEqual(result.City, p.City);
+
+        }
+
+        [Test]
+        public void MyHashtable_Add2_Remove1_succeeds()
+        {
+            var hashtbl = new MyHashtable<string, Person>(10);
+            var p = new Person() { Name = "Louise", City = "Dublin" };
+            var p2 = new Person() { Name = "Mary", City = "Dublin" };
+
+            hashtbl.Add(p.Name, p);
+            hashtbl.Add(p2.Name, p2);
+
+            hashtbl.Remove(p.Name);
+            Assert.AreEqual(hashtbl.Count(), 1);
+
+        }
+
+        [Test]
+        public void MyHashtable_Add2SameKey_Find1_succeeds()
+        {
+            var hashtbl = new MyHashtable<string, Person>(10);
+            var p = new Person() { Name = "Louise", City = "Dublin" };
+            var p2 = new Person() { Name = "Louise", City = "Cork" };
+
+            hashtbl.Add(p.Name, p);
+            hashtbl.Add(p2.Name, p2);
+
+            var result = hashtbl.Find(p2.Name);
+
+            Assert.AreEqual(result.City, p.City);
+            Assert.AreEqual(result.Name, p.Name);
+
+        }
+
+
+        public class Person {
+            public string Name { get; set; }
+            public string City { get; set; }
+        }
+
+    }
+}
